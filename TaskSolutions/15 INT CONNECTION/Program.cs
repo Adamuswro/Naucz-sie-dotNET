139fd7f573b2857a397c62d4f1402bdd3ba1177c
Adam Szulc <adszulc@danfoss.com>
2020-07-30 00:08:31 +0200

    Solution. It is needed to improve max number calculations when first try is out of int size.

--- TaskSolutions/15 INT CONNECTION/Program.cs
+++ TaskSolutions/15 INT CONNECTION/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _15_INT_CONNECTION
 {
@@ -10,6 +8,140 @@ namespace _15_INT_CONNECTION
     {
         static void Main(string[] args)
         {
+            List<int> input = ConvertInput(args);
+
+            if (input == null || input.Count == 0)
+            {
+                CloseProgram($"Error. Wrong input. (Input: {String.Join(" ", args)})");
+
+            }
+            else if (isMoreThanTenDgits(input) == true)
+            {
+                CloseProgram($"Too many digits, result is out of int type size.");
+            }
+            var sortedFromMax = input.
+                OrderByDescending(p => GetDigitFromLeft(p, 1)).
+                ThenByDescending(p => GetDigitFromLeft(p, 2)).
+                ThenByDescending(p => GetDigitFromLeft(p, 3)).
+                ThenByDescending(p => GetDigitFromLeft(p, 4)).
+                ThenByDescending(p => GetDigitFromLeft(p, 5)).
+                ToList();
+
+            var sortedFromMin = input.
+                OrderBy(p => GetDigitFromLeft(p, 1)).
+                ThenBy(p => GetDigitFromLeft(p, 2)).
+                ThenBy(p => GetDigitFromLeft(p, 3)).
+                ThenBy(p => GetDigitFromLeft(p, 4)).
+                ThenBy(p => GetDigitFromLeft(p, 5)).
+                ToList();
+
+            if (Int32.TryParse(String.Join("", sortedFromMin), out int min) == false)
+            {
+                CloseProgram($"Min number is out of int type size.");
+            }
+
+            if (Int32.TryParse(String.Join("", sortedFromMax), out int max) == false)
+            {
+                int bufor;
+                int index = 0;
+                int maxIterations = (sortedFromMax.Count - 1) * (sortedFromMax.Count - 1);
+                for (int i = 0; i < maxIterations; i++)
+                {
+                    if (index == sortedFromMax.Count - 1)
+                    {
+                        index = 0;
+                    }
+
+                    bufor = sortedFromMax[index];
+                    sortedFromMax[index] = sortedFromMax[index + 1];
+                    sortedFromMax[index + 1] = bufor;
+                    if (Int32.TryParse(String.Join("", sortedFromMax), out max) == true)
+                    {
+                        break;
+                    }
+                    index++;
+                }
+            }
+
+            Console.WriteLine($"Input {String.Join(" ", input)}");
+            Console.WriteLine($"Max int: {Int32.MaxValue}");
+            Console.WriteLine($"Max {max}");
+            Console.WriteLine($"Min {min}");
+
+            CloseProgram();
+        }
+
+        private static int LeftToMax(int input)
+        {
+            var digits = GetDigitsNumber(input);
+
+            var max = 10 ^ (digits + 1) - 1;
+
+            return max - input;
+        }
+
+        private static void CloseProgram(string message = "")
+        {
+            if (String.IsNullOrEmpty(message) == false)
+            {
+                Console.WriteLine(message);
+            }
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+        private static bool isMoreThanTenDgits(List<int> input)
+        {
+            int counter = 0;
+
+            foreach (var number in input)
+            {
+                counter += GetDigitsNumber(number);
+            }
+
+            if (counter > 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int GetDigitFromLeft(int input, int numberOfDigitFromLeft)
+        {
+            if (numberOfDigitFromLeft < 1)
+            {
+                throw new ArgumentException("Number of digit must be higher than 0!");
+            }
+
+            int allDigits = GetDigitsNumber(input);
+
+            if (numberOfDigitFromLeft > allDigits)
+            {
+                return GetDigitFromLeft(input, allDigits);
+            }
+
+            int result = (input / (int)Math.Pow(10, GetDigitsNumber(input) - numberOfDigitFromLeft)) % 10;
+            return result;
+        }
+
+        private static int GetDigitsNumber(int input)
+        {
+            return Convert.ToInt32(Math.Floor(Math.Log10(input) + 1));
+        }
+
+        static List<int> ConvertInput(string[] input)
+        {
+            List<int> result;
+            try
+            {
+                result = new List<int>(Array.ConvertAll(input, s => int.Parse(s)));
+            }
+            catch (Exception)
+            {
+                result = new List<int>();
+            }
+            return result;
         }
     }
 }
